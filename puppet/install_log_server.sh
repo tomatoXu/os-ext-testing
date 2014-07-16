fdd6c979cbf40f29a746eb9dc06b6ffc4390b2e8
Ramy Asselin <ramy.asselin@hp.com>
2014-07-15 18:04:38 -0700

    No access to private repo: use values directly

--- puppet/install_log_server.sh
+++ puppet/install_log_server.sh
@@ -6,8 +6,6 @@ set -e
 
 THIS_DIR=`pwd`
 
-DATA_REPO_INFO_FILE=$THIS_DIR/.data_repo_info
-DATA_PATH=$THIS_DIR/os-ext-testing-data
 OSEXT_PATH=$THIS_DIR/os-ext-testing
 OSEXT_REPO=https://github.com/rasselin/os-ext-testing
 PUPPET_MODULE_PATH="--modulepath=$OSEXT_PATH/puppet/modules:/root/config/modules:/etc/puppet/modules"
@@ -33,36 +31,8 @@ if [[ "$PULL_LATEST_OSEXT_REPO" == "1" ]]; then
     cd $OSEXT_PATH; git checkout master && sudo git pull; cd $THIS_DIR
 fi
 
-if [[ ! -e $DATA_PATH ]]; then
-    echo "Enter the URI for the location of your config data repository. Example: https://github.com/jaypipes/os-ext-testing-data"
-    read data_repo_uri
-    if [[ "$data_repo_uri" == "" ]]; then
-        echo "Data repository is required to proceed. Exiting."
-        exit 1
-    fi
-    git clone $data_repo_uri $DATA_PATH
-fi
-
-if [[ "$PULL_LATEST_DATA_REPO" == "1" ]]; then
-    echo "Pulling latest data repo master."
-    cd $DATA_PATH; git checkout master && git pull; cd $THIS_DIR;
-fi
-
-# Pulling in variables from data repository
-. $DATA_PATH/vars.sh
-
-# Validate there is a Jenkins SSH key pair in the data repository
-if [[ -z $JENKINS_SSH_KEY_PATH ]]; then
-    echo "Expected to find JENKINS_SSH_KEY_PATH in $DATA_PATH/vars.sh. Please correct. Exiting."
-    exit 1
-elif [[ ! -e "$DATA_PATH/$JENKINS_SSH_KEY_PATH" ]]; then
-    echo "Expected to find Jenkins SSH key pair at $DATA_PATH/$JENKINS_SSH_KEY_PATH, but wasn't found. Please correct. Exiting."
-    exit 1
-else
-    echo "Using Jenkins SSH key path: $DATA_PATH/$JENKINS_SSH_KEY_PATH"
-    JENKINS_SSH_PRIVATE_KEY_CONTENTS=`sudo cat $DATA_PATH/$JENKINS_SSH_KEY_PATH`
-    JENKINS_SSH_PUBLIC_KEY_CONTENTS=`sudo cat $DATA_PATH/$JENKINS_SSH_KEY_PATH.pub`
-fi
+LOG_SERVER_DOMAIN=csim.hp.com
+JENKINS_SSH_PUBLIC_KEY_CONTENTS="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQCZBfq9HkS9urOg2lRkv+9B3tKDLu9h3QRd43aoMXQLXmivurnGEeVf1mptpWGeMB6E89XC/+xsMM3MxAlTQAuaTEhQ0aZjYpdawToYaj92BvoQRNShvQOOTIeehcZwJzudXu1WLXlv1+0gJIwPswkVqfnn5ptXePh3qhb2jFaZUQ=="
 
 CLASS_ARGS="domain => '$LOG_SERVER_DOMAIN', "
 CLASS_ARGS="$CLASS_ARGS jenkins_ssh_key => '$JENKINS_SSH_PUBLIC_KEY_CONTENTS', "
