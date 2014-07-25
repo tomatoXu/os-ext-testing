e5aa1994e9f816d89399abef943e80a32e22b021
Ramy Asselin <ramy.asselin@hp.com>
2014-07-24 17:28:54 -0700

    Disable use of unbound.
    Remove when this merges in:
    https://review.openstack.org/#/c/96551/

---
+++ puppet/modules/os_ext_testing/files/nodepool/prepare_node.sh
@@ -0,0 +1,106 @@
+#!/bin/bash -xe
+
+# Copyright (C) 2011-2013 OpenStack Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+#
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+HOSTNAME=$1
+SUDO=$2
+THIN=$3
+PYTHON3=${4:-false}
+PYPY=${5:-false}
+ALL_MYSQL_PRIVS=${6:-false}
+GIT_BASE=${7:-git://git.openstack.org}
+ENABLE_UNBOUND=false
+
+sudo hostname $HOSTNAME
+if [ -n "$HOSTNAME" ] && ! grep -q $HOSTNAME /etc/hosts
+then
+    echo "127.0.1.1 $HOSTNAME" | sudo tee -a /etc/hosts
+fi
+
+# Fedora image doesn't come with wget
+if [ -f /usr/bin/yum ]; then
+    sudo yum -y install wget
+fi
+wget https://git.openstack.org/cgit/openstack-infra/config/plain/install_puppet.sh
+sudo bash -xe install_puppet.sh
+
+sudo git clone --depth=1 $GIT_BASE/openstack-infra/config.git \
+    /root/config
+
+sudo cp /opt/nodepool-scripts/*.pp /root/config/modules/openstack_project/manifests
+
+sudo /bin/bash /root/config/install_modules.sh
+if [ -z "$NODEPOOL_SSH_KEY" ] ; then
+    sudo puppet apply --modulepath=/root/config/modules:/etc/puppet/modules \
+        -e "class {'openstack_project::single_use_slave':
+                sudo => $SUDO,
+                thin => $THIN,
+                python3 => $PYTHON3,
+                include_pypy => $PYPY,
+                all_mysql_privs => $ALL_MYSQL_PRIVS,
+                enable_unbound => $ENABLE_UNBOUND,
+            }"
+else
+    sudo puppet apply --modulepath=/root/config/modules:/etc/puppet/modules \
+        -e "class {'openstack_project::single_use_slave':
+                install_users => false,
+                sudo => $SUDO,
+                thin => $THIN,
+                python3 => $PYTHON3,
+                include_pypy => $PYPY,
+                all_mysql_privs => $ALL_MYSQL_PRIVS,
+                ssh_key => '$NODEPOOL_SSH_KEY',
+                enable_unbound => $ENABLE_UNBOUND,
+            }"
+fi
+
+# Make sure DNS works.
+dig git.openstack.org
+
+# Cache all currently known gerrit repos.
+sudo mkdir -p /opt/git
+sudo -i python /opt/nodepool-scripts/cache_git_repos.py $GIT_BASE
+
+# We don't always get ext4 from our clouds, mount ext3 as ext4 on the next
+# boot (eg when this image is used for testing).
+sudo sed -i 's/ext3/ext4/g' /etc/fstab
+
+# Remove additional sources used to install puppet or special version of pypi.
+# We do this because leaving these sources in place causes every test that
+# does an apt-get update to hit those servers which may not have the uptime
+# of our local mirrors.
+OS_FAMILY=$(facter osfamily)
+if [ "$OS_FAMILY" == "Debian" ] ; then
+    sudo rm -f /etc/apt/sources.list.d/*
+    sudo apt-get update
+elif [ "$OS_FAMILY" == "RedHat" ] ; then
+    # Can't delete * in yum.repos.d since all of the repos are listed there.
+    # Be specific instead.
+    if [ -f /etc/yum.repos.d/puppetlabs.repo ] ; then
+        sudo rm -f /etc/yum.repos.d/puppetlabs.repo
+    fi
+fi
+
+# Remove cron jobs
+# We create fresh servers for these hosts, and they are used once. They don't
+# need to do things like update the locatedb or the mandb or rotate logs
+# or really any of those things. We only want code running here that we want
+# here.
+sudo rm -f /etc/cron.{monthly,weekly,daily,hourly,d}/*
+
+sync
+sleep 5
