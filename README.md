a50058dcdbf25410af6e1fa0489214115d5abfd8
Ramy Asselin <ramy.asselin@hp.com>
2015-05-06 15:24:39 -0700

    Switch to using the puppet-openstackci log server
    Remove bug notice for log server
    Update log server installation instructions

--- README.md
+++ README.md
@@ -241,28 +241,17 @@ Jenkins master. To do so manually, follow these steps:
 
 The Log server is a simple VM with an Apache web server installed that provides http access to all the log files uploaded by the jenkins jobs. It is a separate script because the jenkins-zuul-nodepool 'master' server may/can not be publicly accessible for security reasons. In addition, separating out the log server as its own server relaxes the disk space requirements needed by the jenkins master. 
 
-It's configuration uses the openstack-infra scripts, which provide the friendly log filtering features, hightlighting, the line references, etc.
-
-Upstream doesn't currently support Ubuntu 14.04/Apache 2.4. Workaround is to cherry pick this patch or apply it manually to /etc/apache2/sites-enabled/*.conf:
-https://review.openstack.org/#/c/164889/
+It's configuration uses the puppet-openstackci scripts, which provide the friendly log filtering features, hightlighting, the line references, etc.
 
 For simplicity, it is recommended to use the same jenkins key for authentication.
 
 ```
 wget https://raw.githubusercontent.com/rasselin/os-ext-testing/master/puppet/install_log_server.sh
+export DOMAIN=your.domain.com
+export JENKINS_SSH_PUBLIC_KEY=/full/path/to/public/key
 #MANUALLY Update the LOG_SERVER_DOMAIN & JENKINS_SSH_PUBLIC_KEY_CONTENTS variables
 bash install_log_server.sh
 ```
 
-Bug: 
-```
-err: /Stage[main]/Logging::Master/Exec[install_os-loganalyze]: Failed to call refresh: python setup.py install returned 1 instead of one of [0] at /home/stack/os-ext-testing/puppet/modules/logging/manifests/master.pp:89
-```
-Workaround:
-```
-cd /opt/os-loganalyze
-sudo python setup.py install
-```
- 
 When completed, the jenkins user will be able to upload files to /srv/static/logs, which Apache will serve via http.
 
--- puppet/install_log_server.sh
+++ puppet/install_log_server.sh
@@ -4,57 +4,35 @@
 
 set -e
 
-THIS_DIR=`pwd`
 
 # TODO: Either edit the variables here and make sure the values are correct, or
 # set them before running this script
-: ${LOG_SERVER_DOMAIN:=csim.hp.com}
-# No whitespace (i.e. remove "ssh-rsa " identifier and ending comment field)!
-: ${JENKINS_SSH_PUBLIC_KEY_CONTENTS:="AAAAB3NzaC1yc2EAAAADAQABAAAAgQCZBfq9HkS9urOg2lRkv+9B3tKDLu9h3QRd43aoMXQLXmivurnGEeVf1mptpWGeMB6E89XC/+xsMM3MxAlTQAuaTEhQ0aZjYpdawToYaj92BvoQRNShvQOOTIeehcZwJzudXu1WLXlv1+0gJIwPswkVqfnn5ptXePh3qhb2jFaZUQ=="}
+: ${DOMAIN:=your.domain.com}
+: ${JENKINS_SSH_PUBLIC_KEY:="~/.ssh/id_rsa.pub"}
 
-OSEXT_PATH=$THIS_DIR/os-ext-testing
-OSEXT_REPO=https://github.com/rasselin/os-ext-testing
-PUPPET_MODULE_PATH="--modulepath=$OSEXT_PATH/puppet/modules:/root/system-config/modules:/etc/puppet/modules"
+PUPPET_MODULE_PATH="--modulepath=/etc/puppet/modules"
 
-if ! sudo test -d /root/system-config; then
-  sudo git clone https://review.openstack.org/p/openstack-infra/system-config.git \
-    /root/system-config
-fi
-
-if ! sudo test -d /root/project-config; then
-  sudo git clone https://github.com/openstack-infra/project-config.git \
-    /root/project-config
-fi
-
-# Install Puppet and the OpenStack Infra Config source tree
-# TODO(Ramy) Make sure sudo has http proxy settings...
+# Install Puppet
 if [[ ! -e install_puppet.sh ]]; then
   wget https://git.openstack.org/cgit/openstack-infra/system-config/plain/install_puppet.sh
   sudo bash -xe install_puppet.sh
+  sudo git clone https://review.openstack.org/p/openstack-infra/system-config.git \
+    /root/system-config
   sudo /bin/bash /root/system-config/install_modules.sh
 fi
 
-# Update /root/system-config
-echo "Update system-config"
-sudo git  --work-tree=/root/system-config/ --git-dir=/root/system-config/.git remote update
-sudo git  --work-tree=/root/system-config/ --git-dir=/root/system-config/.git pull
+CLASS_ARGS="domain => '$DOMAIN',
+            jenkins_ssh_key => '$(cat ${JENKINS_SSH_PUBLIC_KEY} | cut -d ' ' -f 2)', "
 
-echo "Update project-config"
-sudo git  --work-tree=/root/project-config/ --git-dir=/root/project-config/.git remote update
-sudo git  --work-tree=/root/project-config/ --git-dir=/root/project-config/.git pull
-
-# Clone or pull the the os-ext-testing repository
-if [[ ! -d $OSEXT_PATH ]]; then
-    echo "Cloning os-ext-testing repo..."
-    git clone $OSEXT_REPO $OSEXT_PATH
+set +e
+sudo puppet apply --test $PUPPET_MODULE_PATH -e "class {'openstackci::logserver': $CLASS_ARGS }"
+PUPPET_RET_CODE=$?
+# Puppet doesn't properly return exit codes. Check here the values that
+# indicate failure of some sort happened. 0 and 2 indicate success.
+if [ "$PUPPET_RET_CODE" -eq "4" ] || [ "$PUPPET_RET_CODE" -eq "6" ] ; then
+    echo "Puppet failed to apply the log server configuration."
+    exit $PUPPET_RET_CODE
 fi
+set -e
 
-if [[ "$PULL_LATEST_OSEXT_REPO" == "1" ]]; then
-    echo "Pulling latest os-ext-testing repo master..."
-    cd $OSEXT_PATH; git checkout master && sudo git pull; cd $THIS_DIR
-fi
-
-CLASS_ARGS="domain => '$LOG_SERVER_DOMAIN', "
-CLASS_ARGS="$CLASS_ARGS jenkins_ssh_key => '$JENKINS_SSH_PUBLIC_KEY_CONTENTS', "
-
-sudo puppet apply --verbose $PUPPET_MODULE_PATH -e "class {'logging::master': $CLASS_ARGS }"
+exit 0
